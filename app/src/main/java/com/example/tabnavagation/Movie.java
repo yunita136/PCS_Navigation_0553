3b37eba9a190e3778d625a0d5d80a27b12452052
yunita136 <yunita.1306@students.amikom.ac.id>
2020-11-16 23:50:23 +0700

    PCS_Navigation_0553

---
+++ app/src/main/java/com/example/tabnavagation/Movie.java
@@ -0,0 +1,22 @@
+package com.example.tabnavagation;
+
+public class Movie {
+    private String title;
+    private String year;
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getYear() {
+        return year;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
---
+++ app/src/main/java/com/example/tabnavagation/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.tabnavagation.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tabnavagation.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new PlayingFragment();
+                break;
+            case 1:
+                fragment = new FavoriteFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
